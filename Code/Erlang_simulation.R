45fa6701e8c5b1915bb7891c31ad5415e574a0d9
Ningrui Xie <104842506+Tiffany-Xie@users.noreply.github.com>
2023-10-08 10:10:28 -0400

    Update model & compare

--- Code/Erlang_simulation.R
+++ Code/Erlang_simulation.R
@@ -4,6 +4,7 @@ library(viridis)
 library(ggplot2)
 library(deSolve)
 
+
 # Exact Distribution ####
 erlang <- function(x, n, γ) {
   (n*γ)^n*x^(n-1)*exp(-n*γ*x)/factorial(n-1)
@@ -11,7 +12,7 @@ erlang <- function(x, n, γ) {
 
 plot_erlang <- function(x, n, γ) {
   if (length(n) > 1 & length(γ) > 1) {
-    return("Only one parameter can vary at a time ☺")
+    return("Only one parameter variable everytime :)")
   }
   df <- expand.grid(Time = x, Shape = n, Rate = γ)
   df$P <- with(df, erlang(Time, Shape, Rate))
@@ -45,72 +46,93 @@ plot_erlang <- function(x, n, γ) {
 # Parameters
 x <- seq(0,30, by=0.1)
 n <- c(1,2,3,4,5,10,20,50,100)
-γ <- 1/10
+γ <- 0.077
 #n <- 10
 #γ <- seq(0.1, 0.7, by=0.1)
-#n <- 2
+#n <- 3
 #γ <- 0.1
 plot_erlang(x, n, γ)
 
+#newmodel <- function(t, states, params) {
+#  with(as.list(c(params)), {
+#    boxes <- length(states) - 2
+#    I <- states[1:boxes]
+#    R <- states[boxes+1]
+#    C <- states[boxes+2]
+    
+#    dI <- -(boxes*x+μ)*I
+#    dI[2:boxes] <- dI[2:boxes]
+#  })
+#}
 
-newmodel <- function(t, states, params) {
-  with(as.list(c(params)), {
-		boxes <- length(states) - 2
-		I <- states[1:boxes]
-		R <- states[boxes+1]
-		C <- states[boxes+2]
-
-		dI <- -(boxes*x+μ)*I
-		dI[2:boxes] <- dI[2:boxes]
-	})
-}
-
-# Exact vs. Expect ####
+# Exact vs. Expact ####
 model <- function(t, states, params) {
   with(as.list(c(states, params)), {
-    dI1 <- -(10*γ + μ)*I1
-    dI2 <- 10*γ*I1 - (10*γ + μ)*I2
-    dI3 <- 10*γ*I2 - (10*γ + μ)*I3
-    dI4 <- 10*γ*I3 - (10*γ + μ)*I4
-    dI5 <- 10*γ*I4 - (10*γ + μ)*I5
-    dI6 <- 10*γ*I5 - (10*γ + μ)*I6
-    dI7 <- 10*γ*I6 - (10*γ + μ)*I7
-    dI8 <- 10*γ*I7 - (10*γ + μ)*I8
-    dI9 <- 10*γ*I8 - (10*γ + μ)*I9
-    dI10 <- 10*γ*I9 - (10*γ + μ)*I10
-    dR <- 10*γ*i10 - μ*R
-    dC <-  10*γ*I10
-
-    ## R_delta <- dR
-    list(c(dI1, dI2, dI3, dI4, dI5, dI6, dI7, dI8, dI9, dI10, dR, dC))
+    dI <- numeric(n)
+    #states <- c(states[1], numeric(n-1), states[2])
+    dI[1] <- -(n*γ + μ)*states[1]
+    if (n>1) {
+      for (i in 2:n) {
+        dI[i] <- n*γ*states[i-1] - (n*γ + μ)*states[i]
+      }
+    }
+    dR <- n*γ*states[n] - μ*R
+    list(c(dI, dR))
   })
 }
 
 compare <- function(time, n, γ, μ, states, model) {
+  params <- c(γ = γ, μ = μ, n = n)
   df <- data.frame(Time = time)
-  df$P <- erlang(df$Time, n, γ)
+  df$P <- erlang(time, n, γ)
   soln <- ode(y = states,
               times = time, 
               func = model, 
-              parms = c(γ, μ))
-  df$P_est <- soln[,"R_delta"]
+              parms = params)
+  
+  df$P_est <- c(diff(soln[,"R"])/diff(time), NA)
   ggplot(df, aes(x=Time)) +
     geom_line(aes(y = P, col = "Exact P"), linewidth = 1) +
-    geom_line(aes(y = P_est, col = "Estimated P"), linewidth = 1, linetype = "twodash") +
+    geom_line(aes(y = P_est, col = "Estimated P"), linewidth = 2, linetype = "twodash") +
     labs(title = paste("Probability density functions with 1/γ =", 1/γ, "and n =", n), 
          x = "Stage duration (days)",
          y = "Probability Density")
 }
 
+
 states <- c(I1 = 1, I2 = 0, I3 = 0, 
             I4 = 0, I5 = 0, I6 = 0,
-            I7 = 0, I8 = 0, I9 = 0,
-            I10 = 0, R = 0, C = 0)
-time <- seq(0,30,by=0.1)
-n <- 10
+            I7 = 0, R = 0)
+time <- seq(0,30,by=0.01)
+n <- 7
 γ <- 0.1
 μ <- 0
 compare(time, n, γ, μ, states, model)
+
+
+# Gamma vs. Erlang (check)
+#library(tidyr)
+
+times <- seq(0,30,by=0.01)
+n <- 3
+γ <- 0.1
+df <- data.frame(Time = times,
+                 Gamma = dgamma(times, n, n*γ),
+                 Erlang = erlang(times, n, γ))
+#df <- tidyr::gather(df, key = "Type", value = "PDF", -Time)
+#ggplot(df, aes(x = Time, y = PDF, color = Type)) +
+#  geom_line() +
+#  labs(title = paste("Gamma vs. Erlang, with shape(n) =", n, "and rate(γ) =", γ), 
+#       x = "Time (days)",
+#       y = "Probability Density") +
+#  theme_minimal()
+
+ggplot(df, aes(x=Time)) +
+  geom_line(aes(y = Gamma, col = "Gamma"), linewidth = 1) +
+  geom_line(aes(y = Erlang, col = "Erlang"), linewidth = 2, linetype = "twodash") +
+  labs(title = paste("Gamma vs. Erlang, with shape(n) =", n, "and rate(γ) =", γ), 
+       x = "Stage duration (days)",
+       y = "Probability Density")
 
 
 
