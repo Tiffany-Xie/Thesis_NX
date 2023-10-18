04bcc4486405ec3e18c8f2db3f5ba4e0336033f3
Ningrui Xie <104842506+Tiffany-Xie@users.noreply.github.com>
2023-10-18 17:32:05 -0400

    Comparison

--- Code/Pseudo_Erlang.R
+++ Code/Pseudo_Erlang.R
@@ -22,27 +22,32 @@ SInR_geom <- function(t, states, params) {
 
 # Compare ####
 ES_compare <- function(time, γ, μ, r, nE, nSE, model) {
-  a <- sqrt(nE*γ^2*((1/r^2)^nSE - 1)/(1/r^2-1))
-  params <- c(γ = γ, μ = μ, n = nSE, a = a, r = r)
+  df_E <- data.frame(Time = time)
+  df_E$P_E <- erlang(time, nE, γ)
+  
+  df_SE <- expand.grid(Time = time, nSE= nSE, nE = nE, γ = γ, r = r)
+  df_SE$a <- with(df_SE, sqrt(nE*γ^2*((1/r^2)^nSE - 1)/(1/r^2-1)))
   states <- c(1, numeric(nSE))
-  names(states) <- c(
-    paste ("I", 1:nSE, sep="")
-    , "R"
-  )
+  names(states) <- c(paste ("I", 1:nSE, sep=""), "R")
+  P_SE <- c()
+  for (i in r) {
+    a <- sqrt(nE*γ^2*((1/i^2)^nSE - 1)/(1/i^2-1))
+    params <- c(γ = γ, μ = μ, n = nSE, a = a, r = i)
+    soln <- ode(y = states,
+                times = time, 
+                func = model, 
+                parms = params)
+    P_SE <- c(P_SE, c(diff(soln[,"R"])/diff(time), NA))
+  }
+
+  df_SE$P_SE <- P_SE
   
-  df <- data.frame(Time = time)
-  df$P_E <- erlang(time, nE, γ)
-  
-  soln <- ode(y = states,
-              times = time, 
-              func = model, 
-              parms = params)
-  df$P_SE <- c(diff(soln[,"R"])/diff(time), NA)
-  
-  ggplot(df, aes(x=Time)) +
-    geom_line(aes(y = P_E, col = "Erlang"), linewidth = 1) +
-    geom_line(aes(y = P_SE, col = "Pseudo Erlang"), linewidth = 2, linetype = "twodash") +
-    labs(title = paste0("Probability density functions with D=", 1/γ, ", Erlang stage=", nE, ", Pseudo stage=", nSE), 
+  ggplot(df_SE, aes(x=Time, y=P_SE, color=factor(r))) +
+    geom_line() +
+    scale_color_manual(values = viridis(length(r))) +
+    geom_vline(xintercept = 1/γ, color = "red", linetype = "dashed", linewidth = 1) +
+    geom_line(data=df_E, aes(x = Time, y = P_E), color = "black", linewidth = 1.5) +
+    labs(title = paste0("Compare different r: D=", 1/γ, ", nE=", nE, ", nSE=", nSE),
          x = "Stage duration (days)",
          y = "Probability Density")
 }
@@ -51,12 +56,35 @@ ES_compare <- function(time, γ, μ, r, nE, nSE, model) {
 time = seq(0,30,by=0.01) 
 γ <- 0.1 
 μ <- 0
-r <- 2
+#r <- 2
+r <- c(1.5, 2, 2.5, 3)
 nE <- 4
 nSE <- 12
 
 ES_compare(time, γ, μ, r, nE, nSE, SInR_geom)
 
+
+
+
+#### Try a,r variable cal
+# Example nonlinear system
+equations <- function(x) {
+  y1 <- x[1]^2 + x[2]^2 - 25
+  y2 <- x[1]*x[2] - 9
+  return(c(y1, y2))
+}
+
+# Convert the equations to a formula
+formula <- as.formula(c(y1, y2) ~ x[1] + x[2])
+
+# Initial guess
+initial_guess <- c(x1 = 0, x2 = 0)
+
+# Solve the system of nonlinear equations
+solution <- nls(formula, start = initial_guess)
+
+# Print the result
+print(summary(solution))
 
 
 
