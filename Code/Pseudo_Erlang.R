8ab5cc3dfb05f2b9e5db2caa34ae566b9eb08ad8
Ningrui Xie <104842506+Tiffany-Xie@users.noreply.github.com>
2023-10-24 12:35:05 -0400

    Numerically calculate K

--- Code/Pseudo_Erlang.R
+++ Code/Pseudo_Erlang.R
@@ -45,6 +45,7 @@ EP_compare <- function(time, γ, μ, r, nE, nPE, model) {
   }
 
   df_PE$P_PE <- P_PE 
+  df_PE <- na.omit(df_PE)
   
   p1 <- ggplot(df_PE, aes(x=Time, y=P_PE, color=factor(r))) +
     geom_line() +
@@ -68,32 +69,80 @@ EP_compare <- function(time, γ, μ, r, nE, nPE, model) {
   plots[[1]] <- p1
   plots[[2]] <- p2
   grid.arrange(grobs = plots, ncol = 2)
-  
+  return(df_PE)
 }
 
+num_test <- function(df) {
+  df_K <- data.frame(r = unique(df$r))
+  num_K <- c()
+  for (i in unique(df$r)) {
+    rows <- df[df$r == i, ]
+    data <- rows$P_PE
+    num_K <- c(num_K, var(data)/mean(data)^2)
+    #num_mean <- c(num_mean, 
+    #              sum(ifelse(df$r==i, df$Time*df$P_PE, 0)))
+  }
+  df_K$num_K <- num_K
+  
+  dataE <- erlang(unique(df$Time), df$nE[1], df$γ[1])
+  ggplot(df_K, aes(x=r, y=num_K)) + geom_line() +
+    geom_hline(yintercept=var(dataE)/mean(dataE)^2, color="red", linetype = "dashed", linewidth = 1)+
+    labs(title=paste0("Compare numerically calculated K, nE=", df$nE[1], ", nPE=", df$nPE[1]),
+         y="K")
+}
+
+
+
 # Simulation ####
-time = seq(0,30,by=0.01) 
+time = seq(0,40,by=0.01) 
 γ <- 0.1 
 μ <- 0
 nPE <- 12
 
+# Test r == 1
+r <- 1
+nE <- 12
+states <- c(1, numeric(nPE))
+names(states) <- c(paste ("I", 1:nPE, sep=""), "R")
+
+df <- data.frame(Time = time)
+df$P_E <- erlang(time, nE, γ)
+
+params <- c(γ = γ, μ = μ, n = nPE, a = nPE*γ, r = r)
+soln <- ode(y = states,
+            times = time, 
+            func = SInR_geom, 
+            parms = params)
+df$P_PE <- c(diff(soln[,"R"])/diff(time), NA)
+
+ggplot(df, aes(x=Time)) + 
+  geom_line(aes(y=P_E, col="Exact P"), linewidth=1) +
+  geom_line(aes(y=P_PE, col="ODE"), linewidth = 2, linetype = "twodash") +
+  labs(title = paste0("Check when r=1 with D 0=", 1/γ, ", Erlang stage=", nE, ", Pseudo stage=", nPE, " and r=", r), 
+       x = "Stage duration (days)",
+       y = "Probability Density")
+
+
+
 # nE == 2
 r <- seq(2.5, 3.5, by=0.1)
 nE <- 2
-EP_compare(time, γ, μ, r, nE, nPE, SInR_geom)
+df <- EP_compare(time, γ, μ, r, nE, nPE, SInR_geom)
+num_test(df)
 
 # nE == 4
 r <- seq(1.5, 2.5, by=0.1)
 nE <- 4
-EP_compare(time, γ, μ, r, nE, nPE, SInR_geom)
+df <- EP_compare(time, γ, μ, r, nE, nPE, SInR_geom)
+num_test(df)
 
 # nE == 8
 r <- seq(1.2, 1.30, by=0.01)
 nE <- 8
-EP_compare(time, γ, μ, r, nE, nPE, SInR_geom)
+df <- EP_compare(time, γ, μ, r, nE, nPE, SInR_geom)
+num_test(df)
 
-
-
+# Calculate numerically from the output
 
 
 
