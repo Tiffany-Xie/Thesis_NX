a97214c1c34b75a4469d0c844e89b1f8879f9d8d
Ningrui Xie <104842506+Tiffany-Xie@users.noreply.github.com>
2023-10-30 13:44:26 -0400

    Fixed the problem & Numerically calculated m, K

--- Code/Pseudo_Erlang.R
+++ Code/Pseudo_Erlang.R
@@ -1,6 +1,6 @@
 # Packages ####
 library(viridis)
-library(ggplot2); theme_set(theme_bw(base_size=14))
+library(ggplot2)
 library(deSolve)
 library(gridExtra)
 
@@ -24,16 +24,19 @@ SInR_geom <- function(t, states, params) {
   })
 }
 
-# Compare ####
-EP_compare <- function(time, γ, μ, r, nE, nPE, model) {
-  df_E <- data.frame(Time = time)
-  df_E$P_E <- erlang(time, nE, γ) 
-  
-  df_PE <- expand.grid(Time = time, nPE= nPE, nE = nE, γ = γ, r = r)
-  df_PE$a <- with(df_PE, (1/r^nPE-1)*γ/(1/r-1)) 
+# Functions ####
+Edens <- function(time, γ, nE) {
+  df <- data.frame(Time = time)
+  df$PE <- erlang(time, nE, γ)
+  return(df)
+}
+
+PEdens <- function(time, γ, μ, r, nPE, model) {
+  df <- expand.grid(Time = time, nPE = nPE, γ = γ, r = r)
+  df$a <- with(df, (1/r^nPE-1)*γ/(1/r-1))
   states <- c(1, numeric(nPE))
-  names(states) <- c(paste ("I", 1:nPE, sep=""), "R")
-  P_PE <- c()
+  names(states) <- c(paste0("I", 1:nPE),"R")
+  PPE <- c()
   for (i in r) {
     a <- (1/i^nPE-1)*γ/(1/i-1)
     params <- c(γ = γ, μ = μ, n = nPE, a = a, r = i)
@@ -41,108 +44,123 @@ EP_compare <- function(time, γ, μ, r, nE, nPE, model) {
                 times = time, 
                 func = model, 
                 parms = params)
-    P_PE <- c(P_PE, c(diff(soln[,"R"])/diff(time), NA))
+    PPE <- c(PPE, c(diff(soln[,"R"])/diff(time), NA))
   }
+  df$PPE <- PPE
+  return(na.omit(df))
+}
 
-  df_PE$P_PE <- P_PE 
-  df_PE <- na.omit(df_PE)
-  
-  p1 <- ggplot(df_PE, aes(x=Time, y=P_PE, color=factor(r))) +
+
+Cfdens <- function(dfE, dfPE) {
+  ggplot(dfPE, aes(x=Time, y = PPE, color=factor(r))) + 
     geom_line() +
-    scale_color_manual(values = viridis(length(r))) +
-    geom_vline(xintercept = 1/γ, color = "red", linetype = "dashed", linewidth = 1) +
-    geom_line(data=df_E, aes(x = Time, y = P_E), color = "black", linewidth = 1.5) +
-    labs(title = paste0("Compare different r: D=", 1/γ, ", nE=", nE, ", nPE=", nPE),
+    scale_color_manual(values = viridis(length(unique(r)))) +
+    geom_vline(xintercept = 1/dfPE$γ[1], color = "red", linetype = "dashed", linewidth = 1) +
+    geom_line(data=dfE, aes(x = Time, y = PE), color = "black", linewidth = 1.5) +
+    labs(title = paste0("Compare different r: D=", 1/dfPE$γ[1], ", nE=", nE, ", nPE=", nPE),
          x = "Stage duration (days)",
          y = "Probability Density")
-  
-  df_r <- data.frame(r = r)
-  df_r$a <- with(df_r, (1/r^nPE-1)*γ/(1/r-1))
-  df_r$var <- with(df_r, 1/a^2*(1/r^(2*nPE)-1)/(1/r^2-1))
-  df_r$mean <- with(df_r, 1/a*(1/r^nPE-1)/(1/r-1))
-  df_r$K <- with(df_r, var/mean^2)
-  p2 <- ggplot(df_r, aes(x=r, y=K)) + geom_line() +
+}
+
+
+Kappaf <- function(r, γ, nPE) {
+  df <- data.frame(r = r)
+  df$a <- with(df, (1/r^nPE-1)*γ/(1/r-1))
+  df$mean <- with(df, 1/a*(1/r^nPE-1)/(1/r-1))
+  df$var <- with(df, 1/a^2*(1/r^(2*nPE)-1)/(1/r^2-1))
+  df$K <- with(df, var/mean^2)
+  return(df)
+}
+
+Cfkappa <- function(dfK) {
+  ggplot(dfK, aes(x=r, y=K)) + geom_line() +
     geom_hline(yintercept = 1/(nE), color = "red", linetype = "dashed", linewidth = 1) +
     labs(title="Coefficient of Variation^2 (K)")
-  
-  plots <- list()
-  plots[[1]] <- p1
-  plots[[2]] <- p2
-  grid.arrange(grobs = plots, ncol = 2)
-  return(df_PE)
 }
 
-num_test <- function(df) {
-  df_K <- data.frame(r = unique(df$r))
-  num_K <- c()
-  for (i in unique(df$r)) {
-    rows <- df[df$r == i, ]
-    data <- rows$P_PE
-    num_K <- c(num_K, var(data)/mean(data)^2)
-    #num_mean <- c(num_mean, 
-    #              sum(ifelse(df$r==i, df$Time*df$P_PE, 0)))
+
+Cfnum <- function(dfE, dfPE, r, ts) {
+  Emean = sum(dfE$Time * dfE$PE * ts)
+  Evar = sum(dfE$Time^2 * dfE$PE * ts) - Emean^2
+  EK = Evar/Emean^2
+  dfPEmean <- data.frame(r = r)
+  dfPEK <- data.frame(r = r)
+  tempm <- c()
+  tempK <- c()
+  for (i in r) {
+    rows <- dfPE[dfPE$r == i,]
+    PEmean <- sum(rows$Time * rows$PPE * ts)
+    PEvar <- sum(rows$Time^2 * rows$PPE * ts) - PEmean^2
+    PEK = PEvar/PEmean^2
+    tempm <- c(tempm, PEmean)
+    tempK <- c(tempK, PEK)
   }
-  df_K$num_K <- num_K
+  dfPEmean$PEmean <- tempm
+  dfPEK$PEK <- tempK
   
-  dataE <- erlang(unique(df$Time), df$nE[1], df$γ[1])
-  ggplot(df_K, aes(x=r, y=num_K)) + geom_line() +
-    geom_hline(yintercept=var(dataE)/mean(dataE)^2, color="red", linetype = "dashed", linewidth = 1)+
-    labs(title=paste0("Compare numerically calculated K, nE=", df$nE[1], ", nPE=", df$nPE[1]),
+  p1 <- ggplot(dfPEmean, aes(x=r, y=PEmean)) + geom_line() +
+    geom_hline(yintercept=Emean, color="red", linetype = "dashed", linewidth = 1) +
+    labs(title=paste0("Compare numerically calculated mean, nE=", nE, ", nPE=", nPE),
+         y="mean")
+  
+  p2 <- ggplot(dfPEK, aes(x=r, y=PEK)) + geom_line() +
+    geom_hline(yintercept=EK, color="red", linetype = "dashed", linewidth = 1) +
+    labs(title=paste0("Compare numerically calculated K, nE=", nE, ", nPE=", nPE),
          y="K")
+  grid.arrange(p1, p2, ncol=2)
 }
-
 
 
 # Simulation ####
-time = seq(0,40,by=0.01) 
+time = seq(0,40,by=0.1) 
 γ <- 0.1 
 μ <- 0
 nPE <- 12
-
-# Test r == 1
-r <- 1
-nE <- 12
-states <- c(1, numeric(nPE))
-names(states) <- c(paste ("I", 1:nPE, sep=""), "R")
-
-df <- data.frame(Time = time)
-df$P_E <- erlang(time, nE, γ)
-
-params <- c(γ = γ, μ = μ, n = nPE, a = nPE*γ, r = r)
-soln <- ode(y = states,
-            times = time, 
-            func = SInR_geom, 
-            parms = params)
-df$P_PE <- c(diff(c(soln[,"R"]))/diff(time))
-
-ggplot(df, aes(x=Time)) + 
-  geom_line(aes(y=P_E, col="Exact P"), linewidth=1) +
-  geom_line(aes(y=P_PE, col="ODE"), linewidth = 2, linetype = "twodash") +
-  labs(title = paste0("Check when r=1 with D 0=", 1/γ, ", Erlang stage=", nE, ", Pseudo stage=", nPE, " and r=", r), 
-       x = "Stage duration (days)",
-       y = "Probability Density")
-
-
+ts <- 0.1
 
 # nE == 2
-r <- seq(2.5, 3.5, by=0.1)
+r <- seq(2.5, 3.5, by=ts)
 nE <- 2
-df <- EP_compare(time, γ, μ, r, nE, nPE, SInR_geom)
-num_test(df)
+dfE <- Edens(time, γ, nE)
+dfPE <- PEdens(time, γ, μ, r, nPE, SInR_geom)
+dfK <- Kappaf(r, γ, nPE)
+p1 <- Cfdens(dfE, dfPE)
+p2 <- Cfkappa(dfK)
+grid.arrange(p1, p2, ncol = 2)
+# Num
+Cfnum(dfE, dfPE, r, ts)
 
 # nE == 4
 r <- seq(1.5, 2.5, by=0.1)
 nE <- 4
-df <- EP_compare(time, γ, μ, r, nE, nPE, SInR_geom)
-num_test(df)
+dfE <- Edens(time, γ, nE)
+dfPE <- PEdens(time, γ, μ, r, nPE, SInR_geom)
+dfK <- Kappaf(r, γ, nPE)
+p1 <- Cfdens(dfE, dfPE)
+p2 <- Cfkappa(dfK)
+grid.arrange(p1, p2, ncol = 2)
+# Num
+Cfnum(dfE, dfPE, r, ts)
 
 # nE == 8
 r <- seq(1.2, 1.30, by=0.01)
 nE <- 8
-df <- EP_compare(time, γ, μ, r, nE, nPE, SInR_geom)
-num_test(df)
+dfE <- Edens(time, γ, nE)
+dfPE <- PEdens(time, γ, μ, r, nPE, SInR_geom)
+dfK <- Kappaf(r, γ, nPE)
+p1 <- Cfdens(dfE, dfPE)
+p2 <- Cfkappa(dfK)
+grid.arrange(p1, p2, ncol = 2)
+# Num
+Cfnum(dfE, dfPE, r, ts)
+
 
 # Calculate numerically from the output
+#mean = sum(df$Time * df$P * ts)
+#var = sum(df$Time^2 * df$P * ts) - mean^2
+#K = var/mean^2
+
+
 
 
 
