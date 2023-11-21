227bcb624f56eabc0b965137c7239cf4c0f7409f
Jonathan Dushoff <dushoff@mcmaster.ca>
2023-11-21 12:36:07 -0500

    lunch

--- Code/functions.R
+++ Code/functions.R
@@ -5,6 +5,7 @@ r2kappa <- function(r, n, offset=0){
 	return(kappa - offset)
 }
 
+## Formula version probably not needed
 r2kappaF <- function(r, n, offset=0){
 	return((r^n+1)*(r-1) / ((r^n-1)*(r+1)) - offset)
 }
@@ -19,7 +20,56 @@ kappa2r <- function(kappa, n){
 
 ######################################################################
 
-## Tests
+gammaFlow <- function (mu, kappa, ts, T){
+	steps <- ceiling(T/ts)
+	boundaries <- seq(0, by=ts, length.out=steps+1)
+	cum <- pgamma(boundaries, shape=1/kappa, scale=mu*kappa)
+	return(diff(cum))
+}
+
+flowMoments <- function(flow, ts, offset=1/2){
+	tot <- sum(flow)
+	nflow <- flow/tot
+	times <- ((1:length(flow))-offset)*ts
+	mu <- sum(times*nflow)
+	S <-  sum(times^2*nflow)
+	kap <- S/mu^2-1
+	return(c(tot=tot, mu=mu, kap=kap))
+}
+
+######################################################################
+
+boxIntegrate <- function(time, gam, model) {
+	n <- length(gam)
+	names(states) <- c(paste0("I", 1:n), "R")
+	cum <- ode(y = states,
+		y=c(1, numeric(n))
+		, times = time
+		, func = model,
+		parms = c(n = n, gam=gam)
+	)
+	return(cum)
+}
+
+######################################################################
+
+quit()
+
+ts <- 0.01
+g <- gammaFlow(mu=5, kappa=0.4, ts=ts, T=50)
+print(flowMoments(g, ts=ts, offset=1/2))
+
+g <- gammaFlow(mu=5, kappa=1, ts=ts, T=50)
+print(flowMoments(g, ts=ts, offset=1/2))
+
+g <- gammaFlow(mu=5, kappa=1, ts=ts, T=100)
+print(flowMoments(g, ts=ts, offset=1/2))
+
+######################################################################
+
+quit()
+
+## Test kappa2r and relatives
 
 r2kappa(1, 5)
 r2kappa(2, 10)
