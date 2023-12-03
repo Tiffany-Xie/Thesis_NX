345531026adc0b2902f23e2ec9bd2c98ee4733f9
Ningrui Xie <104842506+Tiffany-Xie@users.noreply.github.com>
2023-12-03 10:27:10 -0500

    Organized 1.0 (add bad method)

--- Code/Erlang_simulation2.0.R
+++ Code/Erlang_simulation2.0.R
@@ -11,7 +11,7 @@ T <- 200
 mu <- 10
 μ <- 0
 
-######################################################################
+###################################################################### GOOD
 
 kappa <- 1/4 ## nE == 4
 ode <- Integration(nPE, mu, kappa, ts, T)
@@ -22,6 +22,21 @@ compPlotDens(gamm, ode, ts, T)
 
 parCheck(gamm, ts, T)
 parCheck(ode, ts, T)
+
+###################################################################### BAD
+
+parCheck_bad <- function(flow, ts, T) {
+  flow <- diff(flow)
+  time <- timeSeq(ts, T, FALSE)[-length(time)] # == +NA
+  tot <- sum(flow)
+  mu <- sum(flow*time)
+  S <- sum(flow*time^2)
+  kappa <- S/mu^2 - 1
+  return(c(tot = tot, mu = mu, kappa = kappa))
+}
+
+parCheck_bad(gamm,ts,T)
+parCheck_bad(ode,ts,T)
  
 ######################################################################
 
