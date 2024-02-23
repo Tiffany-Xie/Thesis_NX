3c683a44432c0dd753cde2b08e08243c4e67c28d
Ningrui Xie <104842506+Tiffany-Xie@users.noreply.github.com>
2024-02-23 12:26:04 -0500

    temp storage

--- Code/fitting.R
+++ Code/fitting.R
@@ -68,9 +68,9 @@ fitW <- simplFit(startPar, fixedPar, df, sir.nll.g)
 print(fitW$fit)
 plotFit(fitW, df, fixedPar, type="SIgR", title = "PE (n=12) -> PE (n=12)")
 
-
+# should I be worried?
 nfit <- 2
-startPar <- list(logβ=-1, logD=2, logkappa=-0.3)
+startPar <- list(logβ=-1, logD=2, logkappa=-0.5)
 fixedPar <- list(n = nfit, μ = μ, N = N, I0 = I0, ts = ts, nbs = nbs, T = T) 
 fitW <- simplFit(startPar, fixedPar, df, sir.nll.g)
 print(fitW$fit)
@@ -140,78 +140,6 @@ plotFit(fitW, df, fixedPar, type="SIgR", title = "E (n=7) -> PE (n=12)")
 
 
 ######################################################################
-# hard test (large kappa) 
-
-kappa <- 0.97 # maximum = 0.97
-nfix <- 12
-
-sigr <- sinnerFlow(β, D, kappa, nfix, μ, N, I0, ts, T)
-df <- simObs(sigr, arp, nbs, seed = 72)
-
-nfit <- 12  
-startPar <- list(logβ=-1, logD=2, logkappa=-0.1)
-fixedPar <- list(n = nfit, μ = μ, N = N, I0 = I0, ts = ts, nbs = nbs, T = T) 
-fitW <- simplFit(startPar, fixedPar, df, sir.nll.g)
-print(fitW$fit) 
-plotFit(fitW, df, fixedPar, type="SIgR", title = "PE (n=12) -> PE (n=12)")
-
-
-nfit <- 6
-startPar <- list(logβ=-1, logD=2, logkappa=-0.1)
-fixedPar <- list(n = nfit, μ = μ, N = N, I0 = I0, ts = ts, nbs = nbs, T = T) 
-fitW <- simplFit(startPar, fixedPar, df, sir.nll.g)
-print(fitW$fit)
-plotFit(fitW, df, fixedPar, type="SIgR", title = "PE (n=12) -> PE (n=6)")
-
-######################################################################
-# error bar plot
-
-β <- 0.2
-D <- 10
-n <- 2
-μ <- 0.01  
-N <- 10000
-I0 <- 10
-ts <- 1
-T <- 200
-nbs<-1000
-arp<-0.9
-
-errplot <- function(kappa, nfix, cnfit) { # cofInterv=0.95
-  sigr <- sinnerFlow(β, D, kappa, nfix, μ, N, I0, ts, T)
-  df <- simObs(sigr, arp, nbs, seed = 73)
-  startPar <- list(logβ=-1, logD=2, logkappa=-0.5)
-  data <- data.frame(variable = integer(),
-                   estimate = numeric(),
-                   lower = numeric(),
-                   upper = numeric())
-  print(data)
-
-  for (n in cnfit) {
-      fixedPar <- list(n = n, μ = μ, N = N, I0 = I0, ts = ts, nbs = nbs, T = T) 
-      fitW <- simplFit(startPar, fixedPar, df, sir.nll.g)
-      
-      log_est_k <- coef(fitW$fit)[["logkappa"]]
-      log_stde_k <- coef(summary(fitW$fit))['logkappa', 'Std. Error']
-      lower <- exp(log_est_k - 1.96*log_stde_k)
-      upper <- exp(log_est_k + 1.96*log_stde_k)
-      data <- rbind(data, data.frame(variable=n, estimate=exp(log_est_k), lower=lower, upper=upper))
-      print(data)
-    }
-  return(data)
-}  
-
-data <- errplot(2/9, 12, c(5,6,7,8,9,10,11,12))
-
-ggplot(data, aes(x=variable, y=estimate)) + 
-  geom_point() +
-  geom_errorbar(aes(ymin=lower, ymax=upper), width=0.2) +
-  geom_hline(yintercept=2/9, col='red', linewidth=1)
-
-
-
-quit()
-######################################################################
 
 # change n doesn't seems to affect fitting a lot
 # same for SIgR model
@@ -220,7 +148,7 @@ quit()
 # only observe somewhat large different when all the other parameters are same
 
 # Erlang -> Pseudo Erlang did not meet expectations
-
+          
 # In mle2(likelihood.m, data = list(obs = obs), start = startPar,  :
 # couldn't invert Hessian
 
