899caa4a2d53654040195289ff2c61c84dc9f2d1
Ningrui Xie <104842506+Tiffany-Xie@users.noreply.github.com>
2024-03-14 22:56:58 -0400

    temp storage

--- Code/Interval.R
+++ Code/Interval.R
@@ -67,6 +67,77 @@ ggplot(df) +
 ######################################################################
 ## gamma -> Erlang
 ######################################################################
+gE.nll <- function(logmean, logkappa, interval) { 
+  ts=max(interval)/length(interval)
+  sinr <- as.data.frame(SInRFlow(β=0, D=exp(logmean), n=round(1/exp(logkappa)), μ=0, 
+                                 N=1, I0=1, 
+                                 ts=ts, T=max(interval)))
+  -sum(dnorm(x=interval, mean=diff(sinr$R)/ts*1000, log=TRUE))
+}
+
+#sinr = as.data.frame(SInRFlow(0,7,4,0,1,1,ts,25))
+#plot(timeSeq(ts,25), diff(sinr$R)/ts*n)
+
+startPar = list(logmean = 2, logkappa = -1)
+fit <- mle2(gE.nll, 
+            data = list(interval = g_interval),
+            start = startPar,
+            method = "Nelder-Mead",
+            control = list(maxit = 10000))
+
+time = seq(0, max(g_interval), length.out=n)
+fitmean = exp(coef(fit)[["logmean"]])
+fitkappa = exp(coef(fit)[["logkappa"]])
+ts = max(g_interval)/length(g_interval)
+df <- data.frame(Time = time,
+                 interval = g_interval,
+                 gamma = dgamma(time, shape=1/kappa, scale=mean*kappa)*n,
+                 fit_gamma = diff(SInRFlow(β=0, D=fitmean, n=round(1/fitkappa), μ=0, 
+                                      N=1, I0=1, 
+                                      ts=ts, T=max(g_interval))[,"R"])/ts*1000)
+ggplot(df) + 
+  geom_histogram(aes(x=interval)) +
+  geom_line(aes(x=Time, y=gamma, color="Gamma"), linewidth=1.5) +
+  geom_line(aes(x=Time, y=fit_gamma, color="Gamma after fit"), linewidth=1.5) +
+  labs(x = "Interval", y = "Count", title = "gamma -> Erlang")
+
+######################################################################
+## gamma -> Pseudo Erlang
+######################################################################
+gPE.nll <- function(logmean, logkappa, interval) { 
+  ts=max(interval)/length(interval)
+  sigr <- as.data.frame(sinnerFlow(β=0, D=exp(logmean), kappa=exp(logkappa), n=7, μ=0, 
+                                 N=n, I0=n, 
+                                 ts=ts, T=max(interval)))
+  -sum(dnorm(x=interval, mean=diff(sigr$R)/ts, log=TRUE))
+}
+
+sigr = sinnerFlow(0,10,0.3,7,0,1000,1000,0.1,25)
+plot(x=timeSeq(0.1,25),y=diff(sigr[,"R"])/0.1,type='l')
+#(β, D, kappa, n, μ, N, I0, ts, T)
+
+startPar = list(logmean = 2, logkappa = -1)
+fit <- mle2(gPE.nll, 
+            data = list(interval = g_interval),
+            start = startPar,
+            method = "Nelder-Mead",
+            control = list(maxit = 10000))
+
+time = seq(0, max(g_interval), length.out=n)
+fitmean = exp(coef(fit)[["logmean"]])
+fitkappa = exp(coef(fit)[["logkappa"]])
+ts = max(g_interval)/length(g_interval)
+df <- data.frame(Time = time,
+                 interval = g_interval,
+                 gamma = dgamma(time, shape=1/kappa, scale=mean*kappa)*n,
+                 fit_gamma = diff(sinnerFlow(β=0, D=fitmean, kappa=fitkappa, n=7, μ=0, 
+                                           N=n, I0=n, 
+                                           ts=ts, T=max(g_interval))[,"R"])/ts)
+ggplot(df) + 
+  geom_histogram(aes(x=interval)) +
+  geom_line(aes(x=Time, y=gamma, color="Gamma"), linewidth=1.5) +
+  geom_line(aes(x=Time, y=fit_gamma, color="Gamma after fit"), linewidth=1.5) +
+  labs(x = "Interval", y = "Count", title = "gamma -> Pseudo Erlang")
 
 
 
