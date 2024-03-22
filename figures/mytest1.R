8936dd3eaf2009cfb5fde48fbf7fe601cce2c352
Ningrui Xie <xien6@mcmaster.ca>
2024-03-22 16:40:18 -0400

    changed mytest1

--- figures/mytest1.R
+++ figures/mytest1.R
@@ -6,6 +6,6 @@ df <- data.frame(A = seq(1,10), B = seq(1,10)^2)
 
 ggplot(df, aes(x = A, y = B)) +
 geom_line() + 
-labs(title = "This is a test plot")
+labs(title = "THIS is a test plot")
 
 # not working qswl....
