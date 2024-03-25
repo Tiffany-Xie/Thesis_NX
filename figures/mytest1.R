752c7bfcec74d3d43c611a8e6e7e767f06e1c511
Ningrui Xie <xien6@mcmaster.ca>
2024-03-25 15:59:11 -0400

    some changes for plot position (√)

--- figures/mytest1.R
+++ figures/mytest1.R
@@ -1,11 +1,12 @@
 library(ggplot2)
 library(shellpipes)
-startGraphics()
+startGraphics(height=4)
+
 
 df <- data.frame(A = seq(1,10), B = seq(1,10)^2)
 
 ggplot(df, aes(x = A, y = B)) +
 geom_line() + 
-labs(title = "THIS is a test plot (TESE MAKE")
+labs(title = "THIS is a test plot (TESE)")
 
 # not working qswl....
