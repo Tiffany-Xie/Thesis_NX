bcc6c1cc855c9fc05cb9f243eca5c3fe13f54fbb
Ningrui Xie <xien6@mcmaster.ca>
2024-03-25 15:23:58 -0400

    test make by changing mytest1

--- figures/mytest1.R
+++ figures/mytest1.R
@@ -6,6 +6,6 @@ df <- data.frame(A = seq(1,10), B = seq(1,10)^2)
 
 ggplot(df, aes(x = A, y = B)) +
 geom_line() + 
-labs(title = "THIS is a test plot")
+labs(title = "THIS is a test plot (TESE MAKE")
 
 # not working qswl....
