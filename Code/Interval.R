7860f8ffde27c2fbbedcab6869b2fc99dd9318f8
Ningrui Xie <xien6@mcmaster.ca>
2024-07-06 22:14:53 +0800

    ramdom gamma -> Erlang & PErlang

--- Code/Interval.R
+++ Code/Interval.R
@@ -9,7 +9,7 @@ library(pseudoErlang)
 
 set.seed(223)
 
-numObs <- 1000
+numObs <- 5000
 kappa <- 0.3
 D <- 7
 
@@ -42,16 +42,16 @@ gg.nll <- function(logmean, logkappa, interval) {
 
 mean = 7
 kappa = 0.3
-n = 1000
+n = 2000
 g_interval = gammaDelay(n,mean,kappa) # v
-startPar = list(logmean = 2, logkappa = -1)
+startPar = list(logmean = 2.5, logkappa = -0.5)
 fit <- mle2(gg.nll, 
             data = list(interval = g_interval),
             start = startPar,
             method = "Nelder-Mead",
             control = list(maxit = 10000))
 
-time = seq(min(g_interval), max(g_interval), length.out=n)
+time = seq(0, max(g_interval), length.out=n)
 fitmean = exp(coef(fit)[["logmean"]])
 fitkappa = exp(coef(fit)[["logkappa"]])
 df <- data.frame(Time = time,
@@ -64,11 +64,11 @@ ggplot(df) +
   geom_line(aes(x=Time, y=fit_gamma, color="Gamma after fit"), linewidth=1.5) +
   labs(x = "Interval", y = "Count", title = "gamma -> gamma")
 
-quit()
+
 ######################################################################
 ## gamma -> Erlang
 ######################################################################
-derlang <- function(x, shape, rate, log=TRUE) { # shape = n; rate = lambda
+derlang <- function(x, shape, rate, log=FALSE) { # shape = n; rate = lambda
   density <- rate^shape * x^(shape-1) * exp(-rate*x) / factorial(shape-1)
   if (log) {
     density <- log(density)
@@ -77,67 +77,97 @@ derlang <- function(x, shape, rate, log=TRUE) { # shape = n; rate = lambda
 }
 
 ge.nll <- function(logmean, logkappa, interval) {
-  -sum(derlang(interval, shape = 1/exp(logkappa), rate = 1/exp(logmean), log=TRUE))
+  -sum(derlang(interval, shape = round(1/exp(logkappa)), rate = 1/exp(logmean)*round(1/exp(logkappa)), log=TRUE))
 }
 
-startPar = list(logmean = 2, logkappa = -1)
+startPar = list(logmean = 2, logkappa = -1) # 2 -1
 fit <- mle2(ge.nll, 
             data = list(interval = g_interval),
             start = startPar,
             method = "Nelder-Mead",
             control = list(maxit = 10000))
 
-time = seq(min(g_interval), max(g_interval), length.out=n)
 fitmean = exp(coef(fit)[["logmean"]])
 fitkappa = exp(coef(fit)[["logkappa"]])
 df <- data.frame(Time = time,
                  interval = g_interval,
                  gamma = dgamma(time, shape=1/kappa, scale=mean*kappa)*n,
-                 fit_gamma = derlang(time, shape=1/fitkappa, rate=1/fitmean)*n)
+                 fit_gamma = derlang(time, shape=round(1/fitkappa), rate=1/fitmean*round(1/fitkappa))*n)
 ggplot(df) + 
   geom_histogram(aes(x=interval)) +
   geom_line(aes(x=Time, y=gamma, color="Gamma"), linewidth=1.5) +
   geom_line(aes(x=Time, y=fit_gamma, color="Erlang after fit"), linewidth=1.5) +
   labs(x = "Interval", y = "Count", title = "gamma -> Erlang")
 
-
+#quit()
 ######################################################################
 ## gamma -> Pseudo Erlang
 ######################################################################
-gPE.nll <- function(logmean, logkappa, interval) { 
-  ts=max(interval)/length(interval)
-  sigr <- as.data.frame(sinnerFlow(β=0, D=exp(logmean), kappa=exp(logkappa), n=7, μ=0, 
-                                 N=n, I0=n, 
-                                 ts=ts, T=max(interval)))
-  -sum(dnorm(x=interval, mean=diff(sigr$R)/ts, log=TRUE))
+
+dperlang <- function(x, mean, kappa, shape=12, log=FALSE) { # shape=n; mean=D
+  r <- kappa2r(kappa, shape)
+  a <- (1-1/r^shape)/(mean*(1-1/r))
+  density <- 0
+  
+  for (i in 1:shape) {
+    innerp <- 1
+    
+    for (j in 1:shape) {
+      if (j != i) {
+        innerp <- innerp * r^(j-1)/(r^(j-1) - r^(i-1))
+      }
+    }
+    density <- density + innerp * a * r^(i-1) * exp(-x*a*r^(i-1))
+  }
+  
+  if (log) {
+    density <- log(density)
+  }
+  return(density)
+} 
+
+gPE.nll <- function(logmean, logkappa, interval) {
+  -sum(dperlang(interval, mean = exp(logmean), kappa = exp(logkappa), log=TRUE))
 }
 
-sigr = sinnerFlow(0,10,0.3,7,0,1000,1000,0.1,25)
-plot(x=timeSeq(0.1,25),y=diff(sigr[,"R"])/0.1,type='l')
-#(β, D, kappa, n, μ, N, I0, ts, T)
-
-startPar = list(logmean = 2, logkappa = -1)
+startPar = list(logmean = 2.6, logkappa = -0.3)
 fit <- mle2(gPE.nll, 
             data = list(interval = g_interval),
             start = startPar,
             method = "Nelder-Mead",
             control = list(maxit = 10000))
 
-time = seq(0, max(g_interval), length.out=n)
 fitmean = exp(coef(fit)[["logmean"]])
 fitkappa = exp(coef(fit)[["logkappa"]])
-ts = max(g_interval)/length(g_interval)
 df <- data.frame(Time = time,
                  interval = g_interval,
                  gamma = dgamma(time, shape=1/kappa, scale=mean*kappa)*n,
-                 fit_gamma = diff(sinnerFlow(β=0, D=fitmean, kappa=fitkappa, n=7, μ=0, 
-                                           N=n, I0=n, 
-                                           ts=ts, T=max(g_interval))[,"R"])/ts)
+                 fit_gamma = dperlang(time, mean=fitmean, kappa=fitkappa)*n)
 ggplot(df) + 
   geom_histogram(aes(x=interval)) +
   geom_line(aes(x=Time, y=gamma, color="Gamma"), linewidth=1.5) +
-  geom_line(aes(x=Time, y=fit_gamma, color="Gamma after fit"), linewidth=1.5) +
-  labs(x = "Interval", y = "Count", title = "gamma -> Pseudo Erlang")
+  geom_line(aes(x=Time, y=fit_gamma, color="Pseudo Erlang after fit"), linewidth=1.5) +
+  labs(x = "Interval", y = "Count", title = "gamma -> PErlang")
+
+## lager n -> (optim) function cannot be evaluated at initial parameters?
+
+quit()
+###############
+kappa=0.3
+mean=7
+time <- timeSeq(1, 50, FALSE)
+df <- data.frame(Time = time,
+                 gamma = dgamma(time, shape=1/kappa, scale=mean*kappa),
+                 perlang = dperlang(time, mean, kappa))
+ggplot(df, aes(x=Time)) +
+  geom_line(aes(y=gamma, color='Gamma'), linewidth=1) +
+  geom_line(aes(y=perlang, color='Pseudo Erlang'), linewidth=1)
+
+num_mean <- sum(df$Time * df$perlang * 1) # ts = 1
+num_var <- sum(df$Time^2 * df$perlang * 1) - num_mean^2
+num_kappa <- num_var / num_mean^2
+num_kappa
+
 
 
 
